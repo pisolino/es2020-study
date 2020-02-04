40e1d7804d13f194035ae5e321792c1313a3753a
pisolino <pisolino@users.noreply.github.com>
2020-02-05 02:42:41 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App hugahoge"></HelloWorld>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  },
+  created() {
+    const userData = {
+      name: "Nagi",
+      address: {
+        city: "Tokyo"
+      }
+    };
+
+    const city = userData.address?.city;
+    console.log(city);
+
+    import('lodash').then((_) => {
+      console.log(_.size(userData));
+    });
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const { VueLoaderPlugin } = require('vue-loader');
+
+module.exports = {
+    // モード値を production に設定すると最適化された状態で、
+    // development に設定するとソースマップ有効でJSファイルが出力される
+    mode: 'development',
+  
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ["vue-style-loader", "css-loader"]
+        },
+  
+        {
+          test: /\.vue$/,
+          loader: "vue-loader"
+        },
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          // Babel のオプションを指定する
+          options: {
+            presets: [
+              // プリセットを指定することで、ES2020 を ES5 に変換
+              '@babel/preset-env',
+            ]
+          }
+        },
+      ]
+    },
+    // import 文で .ts ファイルを解決するため
+    resolve: {
+      // Webpackで利用するときの設定
+      alias: {
+        'vue$': 'vue/dist/vue.esm.js'
+      },
+      extensions: ['*', '.js', '.vue', '.json']
+    },
+    plugins: [
+      // Vueを読み込めるようにするため
+      new VueLoaderPlugin(),
+    ],
+  };
